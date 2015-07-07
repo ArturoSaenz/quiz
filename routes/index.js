142be5c91ccffaa27323bafd694fe30c6b61fc7b
ArturoSaenz <arturosaenz@msn.com>
2015-07-07 10:45:46 +0200

    Despliegue DB en Heroku last

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/quizes',						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
 
-router.get('/quizes/:quizId', quizController.question);
+router.get('/quizes/:quizId', quizController.show);
 router.get('/quizes/:quizId/answer', quizController.answer);
 router.get('/author',authorController.author);
 
